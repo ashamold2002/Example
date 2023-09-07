8465d33a78e12e55479bf3950b217a293e69932d
Asha <asha.dharmaraj@itp.objectfrontier.com>
2023-09-07 12:40:49 +0530

    string array branch is commited

--- Program.cs
+++ Program.cs
@@ -2,3 +2,8 @@
 foreach(int i in arr){
     Console.WriteLine(i);
 }
+
+string[] FriendsName = new string[]{"asha","shneha","laevi","priya"};
+foreach(string str in FriendsName){
+    Console.WriteLine(str);
+}
